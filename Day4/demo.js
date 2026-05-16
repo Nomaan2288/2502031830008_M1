cd901542581a9fda01fbc53186dd7e1c1318ca39
Nomaan2288 <nomanshaikh0652@gmail.com>
2026-05-16 11:50:40 +0530

    uploaded all days

---
+++ Day4/demo.js
@@ -0,0 +1,20 @@
+var x=5;
+var y=6;
+var a=x+y; 
+var b=x-y;
+var c=x*y;
+var d=x/y;
+
+document.getElementById("add").innerHTML="Addition is:"+" "+a;
+document.getElementById("sub").innerHTML="substraction is:"+" "+b;
+document.getElementById("mul").innerHTML="Multiplication is:"+" "+c;
+document.getElementById("div").innerHTML="Division is:"+" "+d;
+
+
+
+
+
+document.writeln("Addition is:"+" "+a +"<br>");
+document.writeln("substraction is:"+" "+b+"<br>");
+document.writeln("Multiplication is:"+" "+c+"<br>");
+document.writeln("Division is:"+" "+d+"<br>");
