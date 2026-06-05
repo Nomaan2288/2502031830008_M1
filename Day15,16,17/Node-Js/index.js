7b76a4c56997bee166b4b122c0be6d33ec7d8b1b
Nomaan2288 <nomanshaikh0652@gmail.com>
2026-06-06 00:46:12 +0530

    Uploaded Day14to17

---
+++ Day15,16,17/Node-Js/index.js
@@ -0,0 +1,59 @@
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+const Contact = require("./models/contact.models")
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/contacts-crud').then(() => console.log("Database Connected."))
+
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended:false}))
+app.use(express.static('public'))
+
+
+app.get('/',async (req, res)=>{
+    const contacts = await Contact.find()
+    res.render('home', {contacts})
+})
+
+app.get('/show-contact/:id', async (req, res)=>{
+    const contact = await Contact.findOne({ _id:req.params.id})
+
+    res.render('show-contact', {contact})
+    })
+
+app.get('/add-contact', (req, res)=>{res.render('add-contact')})
+
+app.post('/add-contact', async (req, res)=>{
+    const contact = await Contact.insertOne({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        phone: req.body.phone,
+        address: req.body.address
+    })
+    res.redirect("/")
+})
+
+app.get('/update-contact/:id', async (req, res)=>{
+    const contact = await Contact.findById(req.params.id)    
+    res.render('update-contact',{ contact })
+})
+
+app.post('/update-contact/:id',async (req, res)=>{
+
+    await Contact.findByIdAndUpdate(req.params.id, req.body)
+    res.redirect('/')
+})
+
+app.get('/delete-contact/:id',async (req, res)=>{
+    await Contact.findByIdAndDelete(req.params.id)
+    res.redirect('/')
+})
+
+app.post('/delete-contact', (req, res)=>{})
+
+app.listen(3000,()=>{
+    console.log("Server started successfully on port : 3000");
+})
